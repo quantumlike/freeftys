ac6aaa182abe8c882a24dce59a715324cc665894
jmgrossma <subayai7@gmail.com>
2017-03-20 16:37:01 -0700

    Translated to English

--- secondary/NppHorizontalRuler/src/resource.h
+++ secondary/NppHorizontalRuler/src/resource.h
@@ -1,37 +1,19 @@
-﻿/*
-This file is:
-The zlib/libpng License 
-Copyright (c) 2012 freesftys<freesftys@gmail.com>
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by NppHorizontalRuler.rc
+//
+#define IDD_DIALOG_RULER_OPTION         100
+#define IDC_RADIO_FONT_EDITOR_SIZE      1004
+#define IDC_COMBO_FIX_FONT_SIZE         1006
+#define IDC_RADIO_FIX_FONT_SIZE         1007
 
-This software is provided 'as-is', without any express or implied warranty. 
-In no event will the authors be held liable for any damages arising from 
-the use of this software.
-
-Permission is granted to anyone to use this software for any purpose, including 
-commercial applications, and to alter it and redistribute it freely, subject to 
-the following restrictions:
-
-    1. The origin of this software must not be misrepresented; you must not 
-	   claim that you wrote the original software. If you use this software in 
-	   a product, an acknowledgment in the product documentation would be 
-	   appreciated but is not required.
-	
-    2. Altered source versions must be plainly marked as such, and must not be 
-       misrepresented as being the original software.
-	
-    3. This notice may not be removed or altered from any source distribution.
-
-原文
-http://opensource.org/licenses/Zlib
-
-日本語訳
-http://sourceforge.jp/projects/opensource/wiki/licenses%2Fzlib_libpng_license
-*/
-#ifndef IDC_STATIC
-#define IDC_STATIC (-1)
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        101
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           101
 #endif
-
-#define IDD_DIALOG_RULER_OPTION                 100
-#define IDC_RADIO_FONT_EDITOR_SIZE              1004
-#define IDC_COMBO_FIX_FONT_SIZE                 1006
-#define IDC_RADIO_FIX_FONT_SIZE                 1007
+#endif
